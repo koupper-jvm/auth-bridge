ad5919adb20f115b237671e9e06b6597f744d932
Jacob <jacob.gacosta@gmail.com>
2021-02-14 10:18:52 -0600

    WIP

--- App/routers/index.js
+++ App/routers/index.js
@@ -1,6 +1,7 @@
 const authenticationManagerController = require('../controllers/authentication');
+const users = require('../controllers/users');
 
 module.exports = (app) => {
     app.post('/login', authenticationManagerController.login);
-    app.post('/signIn', authenticationManagerController.signIn);
+    app.post('/signIn', users.signIn);
 };
