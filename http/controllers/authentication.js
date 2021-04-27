2e2ee45e482b08ca71ecbb38d3d569645cc42153
Jacob G. Acosta <jacob.gacosta@gmail.com>
2021-04-26 23:52:02 -0500

    Update old references to 'user-authentication'

--- http/controllers/authentication.js
+++ http/controllers/authentication.js
@@ -1,9 +1,9 @@
-const AuthenticateUser = new (require('../security/user-authentication'))();
+const Authenticator = new (require('../security/authenticator'))();
 const jwt = require('../security/jwt-generator')
 
 module.exports = {
     login(req, res) {
-        AuthenticateUser.authenticate(req.body.email, req.body.password, res, (user) => {
+        Authenticator.authenticate(req.body.email, req.body.password, res, (user) => {
             req.session.isAuthenticated = true;
             res.setHeader('access_token', jwt(user));
             res.send();
